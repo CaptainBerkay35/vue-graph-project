afe1238e7ee6e749610961e9cff3209126d7971d
Berkay Kaptan <kaptanberkay35@hotmail.com>
2024-02-17 11:38:48 +0300

    Project Setup

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,18 @@
 <template>
-  <div class="home">
+  <div class="bg-red-400 w-32 h-32">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <p>deneme</p>
+    
   </div>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+
 
 @Options({
   components: {
-    HelloWorld,
+    
   },
 })
 export default class HomeView extends Vue {}
