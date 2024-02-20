8d0e543753558fd2bc3d4a5049575b757acfb529
Berkay Kaptan <kaptanberkay35@hotmail.com>
2024-02-20 16:01:06 +0300

    Barchart graph added

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,25 +1,37 @@
 <template>
-  <div>
-    <!-- Burada sayfa içeriği yer alabilir -->
+  <div class="w-full h-3/4 px-2">
+    <BarChart :dailyData="dailyData" />
   </div>
 </template>
 
 <script>
-import { mapActions } from 'vuex';
+import { mapState , mapActions} from 'vuex';
+import BarChart from '@/components/BarChart.vue'; 
 
 export default {
   name: 'HomeView',
+  components: {
+    BarChart
+  },
+  computed: {
+    ...mapState({
+      dailyData: state => state.chartDataModule.dailyData
+    })
+  },
   mounted() {
     this.fetchChartData();
   },
   methods: {
     ...mapActions('chartDataModule', ['fetchDataForChart']),
-    fetchChartData() {
+    async fetchChartData() {
       const marketplace = this.$store.getters['auth/getMarketplace'];
       const sellerId = this.$store.getters['auth/getSellerId'];
-      this.$store.dispatch('chartDataModule/fetchDataForChart', { marketplace, sellerId });
+      try {
+        await this.$store.dispatch('chartDataModule/fetchDataForChart', { marketplace, sellerId });
+      } catch (error) {
+        console.error('Error fetching chart data:', error);
+      }
     }
   }
 }
 </script>
-
